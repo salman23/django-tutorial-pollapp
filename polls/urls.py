cbf8574848488ac0451d2835bf3dd5346e2ad147
Salman Wahed <salman2312@gmail.com>
2014-07-03 21:32:50 +0600

    django app

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,5 @@ from polls import views
 
 urlpatterns = patterns('',
             url(r'^$', views.index, name='index'),
-            url(r'^(?P<poll_id>\d+)$', views.detail, name='detail')
+            url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail')
 )
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello world, This is Poll index")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([p.question for p in latest_poll_list])
+    #return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = RequestContext(request, {'latest_poll_list': latest_poll_list})
+    return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
-    return HttpResponse("You are looking at poll id %s" % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Exception:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
